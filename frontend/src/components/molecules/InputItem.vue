3a64dd51e7312efef95fddaf534d11eef0409a1f
NAOKI NAKANISHI <xnbaxx@gmail.com>
2022-08-11 22:25:47 +0900

    feat: add InputItem.vue to component

---
+++ frontend/src/components/molecules/InputItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="field mb-4">
+    <label class="label is-size-5" v-text="title"></label>
+    <div class="control">
+      <validation-provider v-slot="{ errors }" :name="name" :rules="rules">
+        <base-input
+          v-model="value"
+          :type="name"
+          :name="name"
+          :placeholder="name"
+        />
+        <span class="has-text-danger has-text-weight-bold">{{
+          errors[0]
+        }}</span>
+      </validation-provider>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import BaseInput from '../atoms/BaseInput.vue'
+
+export type DataType = {
+  value: string
+}
+export default Vue.extend({
+  components: {
+    BaseInput,
+  },
+  props: {
+    title: { type: String, default: '' },
+    rules: { type: String, default: '' },
+    type: { type: String, default: '' },
+    name: { type: String, default: '' },
+    placeholder: { type: String, default: '' },
+  },
+  data() {
+    return {
+      value: '',
+    }
+  },
+  watch: {
+    value(newValue: string): void {
+      this.$emit('input', newValue)
+    },
+  },
+})
+</script>
--- frontend/src/components/molecules/RegisterAccountForm.vue
+++ frontend/src/components/molecules/RegisterAccountForm.vue
@@ -2,47 +2,22 @@
   <validation-observer ref="observer" v-slot="{ invalid }">
     <div class="box">
       <h1 class="title has-text-black">アカウント登録</h1>
-      <div class="field mb-4">
-        <label class="label is-size-5">Name</label>
-        <div class="control">
-          <validation-provider
-            v-slot="{ errors }"
-            name="name"
-            rules="required|alphaNum|max:64"
-          >
-            <base-input
-              v-model="name"
-              type="name"
-              name="name"
-              placeholder="name"
-            />
-            <span class="has-text-danger has-text-weight-bold">{{
-              errors[0]
-            }}</span>
-          </validation-provider>
-        </div>
-      </div>
-
-      <div class="field mb-5">
-        <label class="label is-size-5">Password</label>
-        <div class="control">
-          <validation-provider
-            v-slot="{ errors }"
-            name="password"
-            rules="required|alphaNum|max:64"
-          >
-            <base-input
-              v-model="password"
-              type="password"
-              name="password"
-              placeholder="Password"
-            />
-            <span class="has-text-danger has-text-weight-bold">{{
-              errors[0]
-            }}</span>
-          </validation-provider>
-        </div>
-      </div>
+      <input-item
+        title="Name"
+        rules="required|alphaNum|max:64"
+        type="name"
+        name="name"
+        placeholder="name"
+        @input="sendName"
+      />
+      <input-item
+        title="Password"
+        rules="required|alphaNum|max:64"
+        type="password"
+        name="password"
+        placeholder="password"
+        @input="sendPassword"
+      />
 
       <div class="field mb-3">
         <div class="control">
@@ -55,37 +30,25 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { ValidationObserver, ValidationProvider } from 'vee-validate'
-import BaseInput from '../atoms/BaseInput.vue'
+import { ValidationObserver } from 'vee-validate'
 import BaseButton from '../atoms/BaseButton.vue'
+import InputItem from '../molecules/InputItem.vue'
 import { User } from './../../model/User'
 
-export type DataType = {
-  name: string
-  password: string
-}
 export default Vue.extend({
-  components: { BaseInput, BaseButton, ValidationProvider },
+  components: { BaseButton, InputItem },
   props: {
     value: User,
   },
-  data(): DataType {
-    return {
-      name: '',
-      password: '',
-    }
-  },
-  watch: {
-    name(newValue): void {
-      this.value.setName(newValue)
-      this.$emit('input', this.value)
-    },
-    password(newValue): void {
-      this.value.setPassword(newValue)
-      this.$emit('input', this.value)
-    },
-  },
   methods: {
+    sendName(name: string): void {
+      this.value.setName(name)
+      this.$emit('input', this.value)
+    },
+    sendPassword(password: string): void {
+      this.value.setPassword(password)
+      this.$emit('input', this.value)
+    },
     goNext(): void {
       ;(this.$refs.observer as InstanceType<typeof ValidationObserver>).reset()
       this.$emit('click')
