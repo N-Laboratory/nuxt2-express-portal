9fd6d71e48d46daf80d78975a37fb31f64d8806e
NAOKI NAKANISHI <xnbaxx@gmail.com>
2022-08-15 12:19:23 +0900

    feat: add path to router-option.ts

--- frontend/src/plugins/router-option.ts
+++ frontend/src/plugins/router-option.ts
@@ -39,17 +39,28 @@ const resetPasswordComplete: pathList = {
 export default (ctx: Context) => {
   ctx.app.router!.beforeEach(
     (to: Route, from: Route, next: NavigationGuardNext) => {
-      // 以下の場合はstoreのuser情報を削除
-      // 1. アカウント登録完了画面から任意の画面へ遷移
-      // 2. アカウント確認画面からアカウント登録完了・アカウント登録画面以外へ遷移
-      // 3. アカウント登録画面からアカウント確認画面以外へ遷移
       if (
+        // アカウント登録完了画面から任意の画面へ遷移
         from.path === createAccountComplete.to ||
+        // アカウント確認画面からアカウント登録完了・アカウント登録画面以外へ遷移
         (from.path === createAccountConfirm.to &&
           to.path !== createAccountComplete.to &&
           to.path !== createAccountInput.to) ||
+        // アカウント登録画面からアカウント確認画面以外へ遷移
         (from.path === createAccountInput.to &&
-          to.path !== createAccountConfirm.to)
+          to.path !== createAccountConfirm.to) ||
+        // パスワードチェック画面からパスワード登録画面以外へ遷移
+        (from.path === resetPasswordCheck.to &&
+          to.path !== resetPasswordInput.to) ||
+        // パスワード登録画面からパスワード確認画面以外へ遷移
+        (from.path === resetPasswordInput.to &&
+          to.path !== resetPasswordConfirm.to) ||
+        // パスワード確認画面からパスワード変更完了・パスワード登録画面以外へ遷移
+        (from.path === resetPasswordConfirm.to &&
+          to.path !== resetPasswordInput.to &&
+          to.path !== resetPasswordComplete.to) ||
+        // パスワード変更完了画面から任意の画面へ遷移
+        from.path === resetPasswordComplete.to
       ) {
         const storeUser: User = ctx.store.state.user
         if (storeUser && storeUser.name !== '' && storeUser.password !== '') {
