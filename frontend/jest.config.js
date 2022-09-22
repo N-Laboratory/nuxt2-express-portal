cae3eae84565acb0068d46c11ae50dea1296c38b
NAOKI NAKANISHI <xnbaxx@gmail.com>
2022-09-22 21:02:28 +0900

    style: do lintfix

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,12 +5,12 @@ module.exports = {
   },
   preset: 'ts-jest',
   globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
     },
   },
   moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
-  testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
