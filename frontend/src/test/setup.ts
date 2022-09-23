5784b87af1cd83c8236f3dc391df66bdfa121418
“n.nakanishi” <n.nakanishi@xsync-web.jp>
2022-09-23 23:32:44 +0900

    test: export localVue in setup.ts

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,6 +1,11 @@
-import { config } from '@vue/test-utils'
+import { config , createLocalVue } from '@vue/test-utils'
+import { ValidationObserver, ValidationProvider } from 'vee-validate'
 
 // mock font-awesome
 config.stubs['font-awesome-icon'] = {
   template: '<span>font-awesome-icon</span>',
 }
+
+export const localVue = createLocalVue()
+localVue.component('ValidationObserver', ValidationObserver)
+localVue.component('ValidationProvider', ValidationProvider)
