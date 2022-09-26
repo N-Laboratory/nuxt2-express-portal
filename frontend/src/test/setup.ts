635267938b208de7fc77a7f1ecdb1dec970928fd
NAOKI NAKANISHI <xnbaxx@gmail.com>
2022-09-26 15:23:07 +0900

    test: add wait method in setup.ts

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,4 +1,5 @@
 import { config, createLocalVue } from '@vue/test-utils'
+import flushPromises from 'flush-promises';
 import { ValidationObserver, ValidationProvider, extend } from 'vee-validate'
 import * as rules from 'vee-validate/dist/rules'
 import { alphaNum } from '../plugins/vee-validate-custom'
@@ -11,6 +12,12 @@ config.stubs['font-awesome-icon'] = {
 export const localVue = createLocalVue()
 localVue.component('ValidationObserver', ValidationObserver)
 localVue.component('ValidationProvider', ValidationProvider)
+
+export const waitPerfectly = async () => {
+  await flushPromises()
+  jest.runAllTimers()
+  await flushPromises();
+}
 
 // import vee-validate rules
 export const importValidationRules = () => {
