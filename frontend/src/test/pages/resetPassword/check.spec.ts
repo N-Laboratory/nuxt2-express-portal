59df1b45d87683732fc247314972203d932fefbf
n.nakanishi <n.nakanishi@sync-web.jp>
2022-10-30 21:07:42 +0900

    test: create unit-test pages/resetPassword/check

---
+++ frontend/src/test/pages/resetPassword/check.spec.ts
@@ -0,0 +1,125 @@
+import Vuex, { Store } from 'vuex'
+import { shallowMount, Wrapper } from '@vue/test-utils'
+import type { NuxtAxiosInstance } from '@nuxtjs/axios'
+import Sweetalert from 'vue-sweetalert2'
+import { importValidationRules, localVue, waitPerfectly } from '../../setup'
+import { User } from '../../../model/User'
+import { initializeAxios } from '../../../utils/api'
+import Check from '~/pages/resetPassword/check.vue'
+
+jest.useFakeTimers()
+importValidationRules()
+localVue.use(Vuex)
+localVue.use(Sweetalert)
+
+const mutations = { updateUser: jest.fn() }
+const nuxtError = { error: jest.fn() }
+const router = { push: jest.fn() }
+const swal = jest.fn()
+
+let wrapper: Wrapper<Check & { [key: string]: any }>
+let store: Store<{ user: User }>
+
+beforeEach(async () => {
+  store = new Store({
+    state: {
+      user: new User(0, 'Test name', 'Test password'),
+    },
+    mutations,
+  })
+  wrapper = shallowMount(Check, {
+    mocks: {
+      $store: store,
+      $router: router,
+      $nuxt: nuxtError,
+      $swal: swal,
+    },
+    localVue,
+  })
+  await waitPerfectly()
+})
+
+afterEach(() => {
+  wrapper.destroy()
+  mutations.updateUser.mockReset()
+  nuxtError.error.mockReset()
+  router.push.mockReset()
+  swal.mockReset()
+})
+
+test('ユーザー情報が初期値で設定されていること', () => {
+  // Assert
+  expect(wrapper.vm.$data.user.name).toBe('')
+  expect(wrapper.vm.$data.user.password).toBe('')
+})
+
+describe('次へ押下時の動作確認', () => {
+  test('アカウントが存在しない場合は、エラーメッセージ(SweetAlert)が表示されること', async () => {
+    // Arrange
+    initializeAxios({
+      $post: jest.fn().mockResolvedValue(0),
+    } as unknown as NuxtAxiosInstance)
+
+    // Act
+    wrapper.vm.goNext()
+    await waitPerfectly()
+
+    // Assert
+    expect(swal).toHaveBeenCalled()
+  })
+
+  test('アカウントが存在する場合は、Storeにユーザー情報を登録して次画面へ遷移すること', async () => {
+    // Arrange
+    initializeAxios({
+      $post: jest.fn().mockResolvedValue(1),
+    } as unknown as NuxtAxiosInstance)
+
+    // Act
+    wrapper.vm.goNext()
+    await waitPerfectly()
+
+    // Assert
+    expect(swal).not.toHaveBeenCalled()
+    expect(wrapper.vm.$data.user.id).toBe(1)
+    expect(mutations.updateUser).toHaveBeenCalled()
+    expect(router.push).toBeCalledWith('input')
+  })
+
+  test('次画面への遷移に失敗した場合はエラー画面へ遷移すること', async () => {
+    // Arrange
+    initializeAxios({
+      $post: jest.fn().mockResolvedValue(1),
+    } as unknown as NuxtAxiosInstance)
+    wrapper = shallowMount(Check, {
+      mocks: {
+        $store: store,
+        $router: { push: null },
+        $nuxt: nuxtError,
+        $swal: swal,
+      },
+      localVue,
+    })
+
+    // Act
+    wrapper.vm.goNext()
+    await waitPerfectly()
+
+    // Assert
+    expect(nuxtError.error).toHaveBeenCalled()
+  })
+
+  test('axiosのHTTP通信に失敗した場合はエラー画面へ遷移すること', async () => {
+    // Arrange
+    initializeAxios({
+      // eslint-disable-next-line prefer-promise-reject-errors
+      $post: jest.fn().mockResolvedValue(Promise.reject()),
+    } as unknown as NuxtAxiosInstance)
+
+    // Act
+    wrapper.vm.goNext()
+    await waitPerfectly()
+
+    // Assert
+    expect(nuxtError.error).toHaveBeenCalled()
+  })
+})
