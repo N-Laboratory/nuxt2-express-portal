8d9ed88ab0984bbf083aba3a43a5b2c1000d5b76
n.nakanishi <n.nakanishi@sync-web.jp>
2022-10-30 21:20:02 +0900

    test: create unit-test pages/resetPassword/complete

---
+++ frontend/src/test/pages/resetPassword/complete.spec.ts
@@ -0,0 +1,28 @@
+import Vuex, { Store } from 'vuex'
+import { shallowMount } from '@vue/test-utils'
+import { localVue, waitPerfectly } from '../../setup'
+import { User } from '../../../model/User'
+import Complete from '~/pages/resetPassword/complete.vue'
+
+jest.useFakeTimers()
+localVue.use(Vuex)
+
+test('ユーザー情報がStoreに保存されている情報で設定されていること', async () => {
+  // Arrange
+  const store = new Store({
+    state: {
+      user: new User(0, 'Test Name', 'Test Password'),
+    },
+  })
+  const wrapper = shallowMount(Complete, {
+    mocks: {
+      $store: store,
+    },
+    localVue,
+  })
+  await waitPerfectly()
+
+  // Assert
+  expect(wrapper.vm.$data.user.name).toBe('Test Name')
+  expect(wrapper.vm.$data.user.password).toBe('Test Password')
+})
