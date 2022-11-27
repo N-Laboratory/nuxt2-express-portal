f286d18653bc47d5cddb5135279f1f28d5d1bf34
n.nakanishi <n.nakanishi@sync-web.jp>
2022-11-27 18:55:09 +0900

    style: do lintfix

--- frontend/src/e2e/test/contents/about.spec.ts
+++ frontend/src/e2e/test/contents/about.spec.ts
@@ -1,10 +1,16 @@
 it('aboutページが表示されること', async () => {
   // Act
   await page.goto('http://localhost:3030/contents/about')
-  await page.waitForNavigation({ timeout: 5000, waitUntil: "domcontentloaded" });
+  await page.waitForNavigation({ timeout: 5000, waitUntil: 'domcontentloaded' })
 
-  const existsPage = await page.$eval('[data-testid="about-page"]', (element) => !!element)
-  await page.screenshot({ path: `./src/e2e/evidence/contents/about.png`, fullPage: true })
+  const existsPage = await page.$eval(
+    '[data-testid="about-page"]',
+    (element) => !!element
+  )
+  await page.screenshot({
+    path: `./src/e2e/evidence/contents/about.png`,
+    fullPage: true,
+  })
 
   // Assert
   expect(existsPage).toBeTruthy()
--- frontend/src/e2e/types/global.d.ts
+++ frontend/src/e2e/types/global.d.ts
@@ -1,7 +1,7 @@
-import { Browser, Page } from 'puppeteer';
+import { Browser, Page } from 'puppeteer'
 
 // browser, pageの型定義を追加して、globalで使用できるようにする。
 declare global {
-  const browser: Browser;
-  const page: Page;
+  const browser: Browser
+  const page: Page
 }
