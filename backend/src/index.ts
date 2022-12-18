86320731029a41b2ecccde9873cc70ceee46f57a
n.nakanishi <n.nakanishi@sync-web.jp>
2022-12-18 16:48:24 +0900

    refactor: add CORS setting to express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,23 @@ AppDataSource.initialize()
     app.use(json())
     app.use(helmet())
 
+    const allowCrossDomain = function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, access_token'
+      )
+
+      // intercept OPTIONS method
+      if ('OPTIONS' === req.method) {
+        res.send(200)
+      } else {
+        next()
+      }
+    }
+    app.use(allowCrossDomain)
+
     // register express routes from defined application routes
     Routes.forEach((route) => {
       ;(app as any)[route.method](
